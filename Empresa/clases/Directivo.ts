bbf81b58f51c7d2811b8befdd15941b98e0a972c
Wilman Sirit <wsirit@gmail.com>
2022-08-22 13:19:27 -0400

    Modificando la clase Directivo. Eliminando el constructor sin uso

--- Empresa/clases/Directivo.ts
+++ Empresa/clases/Directivo.ts
@@ -4,14 +4,8 @@ export class Directivo extends Empleado {
 
     subordinados:Empleado[] = [];
 
-    constructor(nombres:string, edad:number, cargo:string, sueldo:number){
-        super(nombres, edad, cargo, sueldo);
-    }
-
-    agregarSubordinado(empleado:Empleado): void {
-        
-        this.subordinados.push(empleado)
-       
+    agregarSubordinado(empleado:Empleado): void {        
+        this.subordinados.push(empleado)       
     }
 
     mostrarSubordinados(): void {
